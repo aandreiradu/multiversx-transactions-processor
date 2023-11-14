b11c3c6394aa3c148084122c9365c1a98b723bc4
Andrei Radu <raduandrei697@gmail.com>
2023-11-15 01:26:51 +0200

    init watcher

--- apps/watcher/src/watcher.controller.ts
+++ apps/watcher/src/watcher.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { WatcherService } from './watcher.service';
+import { WalletRepository } from '@app/common/wallet/wallet.repository';
+import { Wallet } from '@prisma/client';
+import {
+  addressDTO,
+  availableCoinsDTO,
+  addressIdSchema,
+  availableCoinsSchema,
+} from './dtos';
+import { ZodPipe } from './common/pipes/zodPipe';
+import { TransactionsRepository } from '@app/common';
 
-@Controller()
+@Controller('balances/coins')
 export class WatcherController {
-  constructor(private readonly watcherService: WatcherService) {}
+  constructor(
+    private readonly watcherService: WatcherService,
+    private readonly walletRepository: WalletRepository,
+    private readonly transactionsRepository: TransactionsRepository,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.watcherService.getHello();
+  async getHello(): Promise<Wallet> {
+    return this.walletRepository.checkWalletExistence(
+      'erd1qqqqqqqqqqqqqpgq9nnx7n40snv899ejwcrtepc5qn6apxvm2jps88s0v7',
+    );
+  }
+
+  // @Get(':coin/received')
+  // async signIn(@Param() signInParams: RetrieveAmountsDTO) {
+  //   return signInParams;
+  // }
+
+  @Get(':coin/received')
+  async test(
+    @Param(new ZodPipe(availableCoinsSchema)) { coin }: availableCoinsDTO,
+    @Query(new ZodPipe(addressIdSchema)) { addressId }: addressDTO,
+  ) {
+    return this.transactionsRepository.getWalletSentAmount(addressId, coin);
   }
 }
--- apps/watcher/src/watcher.module.ts
+++ apps/watcher/src/watcher.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { WatcherController } from './watcher.controller';
 import { WatcherService } from './watcher.service';
+import { WalletRepository } from '@app/common/wallet/wallet.repository';
+import {
+  PrismaModule,
+  PrismaService,
+  TransactionsRepository,
+  validate,
+} from '@app/common';
+import { CustomRedisService } from '@app/common/redis/redis.service';
+import { CustomRedisModule } from '@app/common/redis/redis.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validate,
+    }),
+    PrismaModule,
+    CustomRedisModule,
+  ],
   controllers: [WatcherController],
-  providers: [WatcherService],
+  providers: [
+    ConfigService,
+    WatcherService,
+    PrismaService,
+    CustomRedisService,
+    WalletRepository,
+    TransactionsRepository,
+  ],
 })
 export class WatcherModule {}
