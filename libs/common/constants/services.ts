4556ed08b267a4fb7f49bd223d8fd4cebcfe401b
Andrei Radu <raduandrei697@gmail.com>
2023-11-16 11:07:26 +0200

    chore: docker fix + cl-up

--- libs/common/constants/services.ts
+++ libs/common/constants/services.ts
@@ -3,26 +3,3 @@ export const TRANSACTIONS_SERVICE = 'MVX_TRANSACTIONS';
 export const receiverCacheFormat = `receiver:{walletId}:coin:{coinType}`;
 export const senderCacheFormat = `sender:{walletId}:coin:{coinType}`;
 export const cachedWalletFormat = `wallet:{walletId}`;
-
-/**
- * 
- * const walletId = 123;
-const coinType = 'EGLD';
-
-// Fetch transaction amounts for the receiver (walletId) on the coin (EGLD)
-const receiverKey = `receiver:${walletId}:coin:${coinType}:amount`;
-const receivedAmounts = await customRedisService.get<number[]>(receiverKey);
-
-// Calculate the total received amount for the wallet on EGLD
-let totalReceivedAmount = 0;
-
-if (receivedAmounts) {
-  // Sum only the amounts received by the wallet
-  totalReceivedAmount = receivedAmounts.reduce((acc, amount) => acc + amount, 0);
-}
-
-// Return the total received amount for the wallet on EGLD
-return totalReceivedAmount;
-
- * 
- */
